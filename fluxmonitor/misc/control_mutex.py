d2321ed2acaaf781efe634fba911c612dd1f5312
Cerberus <cerberus1220@gmail.com>
2015-04-29 15:19:12 +0800

    Missing file

---
+++ fluxmonitor/misc/control_mutex.py
@@ -0,0 +1,63 @@
+
+from errno import EAGAIN
+import fcntl
+import os
+
+from fluxmonitor.config import general_config
+
+"""
+control_mutex use file lock to prevent two program run at sametime
+"""
+
+
+def _get_control_file():
+    return os.path.join(general_config["db"], "control.pid")
+
+
+def locking_status():
+    """
+    return [pid, label]
+
+    Return control program when pid and label, if no program is running, return
+    [0, None]
+    """
+
+    fn = _get_control_file()
+    if os.path.isfile(fn):
+        try:
+            with open(fn, "a+") as f:
+                 fcntl.lockf(f.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
+                 fcntl.lockf(f.fileno(), fcntl.LOCK_UN)
+                 return 0, None
+        except IOError:
+            with open(fn, "r") as f:
+                pid, label = f.read().split("\n", 1)
+                return int(pid, 10), label.strip()
+    else:
+        return 0, None
+
+
+class ControlLock(object):
+    def __init__(self, program_label):
+        self.label = program_label
+        self.mutex_file = _get_control_file()
+
+    def __enter__(self):
+        self.f = f = open(self.mutex_file, 'w')
+        try:
+            fcntl.lockf(f.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
+        except IOError as e:
+            if e.args[0] == EAGAIN:
+                raise RuntimeError("Another control program is running")
+            else:
+                raise
+
+        f.write("%i\n%s\n" % (os.getpid(), self.label))
+        f.flush()
+
+        return self
+
+    def __exit__(self ,type, value, traceback):
+        fcntl.lockf(self.f.fileno(), fcntl.LOCK_UN)
+        self.f.close()
+        os.unlink(self.mutex_file)
