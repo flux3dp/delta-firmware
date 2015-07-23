ef235df2c357ba47641fdd0095393282d45d46cc
Cerberus <cerberus1220@gmail.com>
2015-07-23 18:29:37 +0800

    Add USB Serial GPIO Control

--- fluxmonitor/hal/uart/raspberry_1.py
+++ fluxmonitor/hal/uart/raspberry_1.py
@@ -3,6 +3,7 @@ from time import sleep
 import logging
 
 from serial import Serial
+from RPi import GPIO
 
 from fluxmonitor.misc.async_signal import AsyncIO
 from fluxmonitor.halprofile import MODEL_G1
@@ -10,10 +11,44 @@ from fluxmonitor.config import hal_config
 
 from .base import UartHalBase, BaseOnSerial
 
-logger = logging.getLogger("hal.uart.smoothie")
+L = logging.getLogger("hal.uart.rasp")
+
+GPIO_USB_SERIAL = 15
+USB_SERIAL_ON = GPIO.HIGH
+USB_SERIAL_OFF = GPIO.LOW
 
 
-class UartHal(UartHalBase, BaseOnSerial):
+class AMA0Routing(object):
+    __gpio_val__ = USB_SERIAL_OFF
+    head_enabled = True
+
+    def init_ama0_routing(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(GPIO_USB_SERIAL, GPIO.OUT)
+        L.debug("GPIO configured")
+
+        self.update_ama0_routing()
+
+    def __del__(self):
+        L.debug("GPIO cleanup")
+        GPIO.cleanup()
+
+    def update_ama0_routing(self):
+        if len(self.headboard_socks) > 0:
+            if self.__gpio_val__ == USB_SERIAL_ON:
+                L.debug("Headboard ON / USB OFF")
+                self.head_enabled = True
+                self.__gpio_val__ = USB_SERIAL_OFF
+                GPIO.output(GPIO_USB_SERIAL, self.__gpio_val__)
+        else:
+            if self.__gpio_val__ == USB_SERIAL_OFF:
+                L.debug("Headboard OFF / USB ON")
+                self.head_enabled = False
+                self.__gpio_val__ = USB_SERIAL_ON
+                GPIO.output(GPIO_USB_SERIAL, self.__gpio_val__)
+
+
+class UartHal(UartHalBase, BaseOnSerial, AMA0Routing):
     mainboard_uart = raspi_uart = None
     mainboard_io = raspi_io = None
 
@@ -22,11 +57,11 @@ class UartHal(UartHalBase, BaseOnSerial):
 
     def __init__(self, server):
         super(UartHal, self).__init__(server)
+        self.init_ama0_routing()
         self._connect()
 
     def on_recvfrom_raspi_io(self, obj):
-        # TODO: NOT READY!
-        if self.headboard_socks:
+        if self.head_enabled:
             self.on_recvfrom_headboard(obj)
         else:
             self.on_recvfrom_pc(obj)
@@ -35,14 +70,14 @@ class UartHal(UartHalBase, BaseOnSerial):
         self.mainboard_uart.write(buf)
 
     def sendto_headboard(self, buf):
-        # TODO: NOT READY!
-        for c in buf:
-            self.raspi_uart.write(c)
-            sleep(0.02)
+        if self.head_enabled:
+            for c in buf:
+                self.raspi_uart.write(c)
+                sleep(0.02)
 
     def sendto_pc(self, buf):
-        # TODO: NOT READY!
-        self.raspi_uart.write(buf)
+        if not self.head_enabled:
+            self.raspi_uart.write(buf)
 
     def reconnect(self):
         self._disconnect()
@@ -51,8 +86,8 @@ class UartHal(UartHalBase, BaseOnSerial):
     def _connect(self):
         self.mainboard_uart = Serial(port=hal_config["mainboard_uart"],
                                      baudrate=115200, timeout=0)
-        # TODO: baudrate adjust to 28800
-        self.raspi_uart = Serial(port=hal_config["headboard_uart"],
+
+        self.raspi_uart = Serial(port="/dev/ttyAMA0",
                                  baudrate=115200, timeout=0)
 
         self.mainboard_io = AsyncIO(self.mainboard_uart,
@@ -79,3 +114,11 @@ class UartHal(UartHalBase, BaseOnSerial):
                 self.raspi_uart = None
             except Exception:
                 pass
+
+    def on_connected_headboard(self, sender):
+        UartHalBase.on_connected_headboard(self, sender)
+        self.update_ama0_routing()
+
+    def on_disconnect_headboard(self, ref):
+        UartHalBase.on_disconnect_headboard(self, ref)
+        self.update_ama0_routing()
